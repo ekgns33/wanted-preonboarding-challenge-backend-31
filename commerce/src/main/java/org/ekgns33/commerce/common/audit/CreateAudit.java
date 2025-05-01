b3dc52325f1a52d2d3e6ce9b9743589624742a72
ekgns33 <ekgns00421@gmail.com>
2025-05-02 00:36:44 +0900

    feat : create jpa-audit

---
+++ commerce/src/main/java/org/ekgns33/commerce/common/audit/CreateAudit.java
@@ -0,0 +1,18 @@
+package org.ekgns33.commerce.common.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class CreateAudit {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ commerce/src/main/java/org/ekgns33/commerce/common/config/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package org.ekgns33.commerce.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {}
