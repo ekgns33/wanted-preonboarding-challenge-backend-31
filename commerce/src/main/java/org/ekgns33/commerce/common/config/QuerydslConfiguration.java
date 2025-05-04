b3ac3b5532187dcad7088de2a617ae904a049a6c
ekgns33 <ekgns00421@gmail.com>
2025-05-05 00:32:07 +0900

    chore : configure querydsl

---
+++ commerce/src/main/java/org/ekgns33/commerce/common/config/QuerydslConfiguration.java
@@ -0,0 +1,19 @@
+package org.ekgns33.commerce.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfiguration {
+
+  @Autowired
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory queryFactory() {
+    return new JPAQueryFactory(entityManager);
+  }
+}
